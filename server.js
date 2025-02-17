ce7aa461b661427a3f16810f98632d3a3e024377
Saravanaanandh <saravanaanandh2006@gmail.com>
2025-02-17 10:42:01 +0530

    cookie sameSite updated to None

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-    origin:"*",
+    origin:"http://127.0.0.1:5500",
     methods: "GET,POST,PATCH,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials:true
